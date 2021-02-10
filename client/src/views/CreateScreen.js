559cb57b5a371695a818335a95a1fafc06d1ed82
Adrien Obert <a.obert@sfereno.com>
2021-02-10 11:20:12 +0100

    Add post file form

--- client/src/views/CreateScreen.js
+++ client/src/views/CreateScreen.js
@@ -1,9 +1,31 @@
+import { Button, Form, Input } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
 import React, { Component } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
 
 class CreateScreen extends Component {
+  state = {
+    fileEvent: null,
+  };
+  
+  handleChange = (e) => {
+    this.setState({fileEvent: e.target.files[0]});
+  }
+  
+  prepareForSend = (values) => {
+    
+    let fd = new FormData();
+    
+    for (const [key, value] of Object.entries(values)) {
+        if (value !== undefined) fd.set(key, value)
+        if (key === 'file' && value !== undefined) fd.set(key, this.state.fileEvent)
+    }
+
+    return fd
+}
+
   onFinish = (values) => {
-    console.log('Success:', values);
+    let newVal = this.prepareForSend(values)
+    console.log('newVal:', newVal);
   };
 
   onFinishFailed = (errorInfo) => {
@@ -12,35 +34,27 @@ class CreateScreen extends Component {
 
   render() {
     return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={this.onFinish}
-      onFinishFailed={this.onFinishFailed}
-    >
-      <Form.Item
-        label="Name"
-        name="name"
-        rules={[{ required: true, message: 'Please input token name !' }]}
-      >
-        <Input />
-      </Form.Item>
+      <Form {...layout} encType="multipart/form-data" name="basic" initialValues={{ remember: true }} onFinish={this.onFinish} onFinishFailed={this.onFinishFailed} >
+        <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input token name !' }]} >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Price"
-        name="price"
-        rules={[{ required: true, message: 'Please input token price (Ether) !' }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item label="Price" name="price" rules={[{ required: true, message: 'Please input token price (Ether) !' }]} >
+          <Input type="number" />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item label="File" name="file" rules={[{ required: true, message: 'Please input token file (Ether) !' }]} >
+          <Input type="file" onChange={this.handleChange} />
+        </Form.Item>
+
+        <Form.Item label="Description" name="description" rules={[{ required: true, message: 'Please input token description (Ether) !' }]} >
+          <TextArea />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}> 
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
@@ -49,16 +63,16 @@ export default CreateScreen;
 
 const layout = {
   labelCol: {
-    span: 8,
+    span: 4,
   },
   wrapperCol: {
-    span: 16,
+    span: 8,
   },
-  flex : 1
+  flex: 1
 };
 const tailLayout = {
   wrapperCol: {
-    offset: 8,
-    span: 16,
+    offset: 4,
+    span: 8,
   },
 };
