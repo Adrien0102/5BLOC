3df0828a329c2a387df0e79b583e687c4ad3ab52
dkarol <dimitri.karol@actimage.com>
2021-02-10 10:03:54 +0100

    add create form

--- client/src/views/CreateScreen.js
+++ client/src/views/CreateScreen.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 
 class CreateScreen extends Component {
+  onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   render() {
     return (
-      <div> CREATE </div>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={this.onFinish}
+      onFinishFailed={this.onFinishFailed}
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: 'Please input token name !' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Price"
+        name="price"
+        rules={[{ required: true, message: 'Please input token price (Ether) !' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     );
   }
 }
 
 export default CreateScreen;
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+  flex : 1
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
